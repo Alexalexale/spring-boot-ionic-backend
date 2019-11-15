28e93a6b4a468c291da413a7888d2bb0286ea773
Alessandro Duarte <alessandro.junior@iteris.com.br>
2019-11-15 14:53:05 -0200

    change database

--- src/main/java/com/example/main/PedidoApplication.java
+++ src/main/java/com/example/main/PedidoApplication.java
@@ -1,24 +1,20 @@
 package com.example.main;
 
 import org.modelmapper.ModelMapper;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class PedidoApplication implements CommandLineRunner {
+public class PedidoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PedidoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PedidoApplication.class, args);
+    }
 
-	@Bean
-	public ModelMapper modelMapper() {
-		return new ModelMapper();
-	}
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 
-	@Override
-	public void run(String... args) throws Exception {
-	}
 }
